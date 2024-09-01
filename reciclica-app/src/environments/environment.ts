8473a0b5fa74067196a3dd077203d3c1f4c0d771
galuhazzahraaa <galuhazzhrc3107@gmail.com>
2024-09-02 02:21:19 +0700

    first commit

---
+++ reciclica-app/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyAhb0b7410VV18PjDAEoKLHZCpLOBK2ikY",
+    authDomain: "reciclica-b032f.firebaseapp.com",
+    projectId: "reciclica-b032f",
+    storageBucket: "reciclica-b032f.appspot.com",
+    messagingSenderId: "661883050166",
+    appId: "1:661883050166:web:77e93cd29571f3389fee17"
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
